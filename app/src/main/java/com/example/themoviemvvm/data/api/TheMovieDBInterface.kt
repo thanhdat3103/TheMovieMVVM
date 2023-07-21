0e7f50349b0d1095e026cc06bcbadb25ed57ec1b
thanhdat3103 <atnguyn79@gmail.com>
2023-07-21 15:28:19 +0700

    Creating The UI for Popular Movie

--- app/src/main/java/com/example/themoviemvvm/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/example/themoviemvvm/data/api/TheMovieDBInterface.kt
@@ -1,12 +1,16 @@
 package com.example.themoviemvvm.data.api
 
 import com.example.themoviemvvm.data.value_object.MovieDetails
+import com.example.themoviemvvm.data.value_object.MoviePopularResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
 
+    @GET("move/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MoviePopularResponse>
     @GET("movie/{movie_id}")  //upcoming , popular
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
 
