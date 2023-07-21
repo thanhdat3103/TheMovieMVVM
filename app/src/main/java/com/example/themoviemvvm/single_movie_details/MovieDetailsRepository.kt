2a6dbe94f62492431b3703335f6d4f7056942313
thanhdat3103 <atnguyn79@gmail.com>
2023-07-21 13:50:32 +0700

    Fetching Data with RxJava

---
+++ app/src/main/java/com/example/themoviemvvm/single_movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,24 @@
+package com.example.themoviemvvm.single_movie_details
+
+import androidx.lifecycle.LiveData
+import com.example.themoviemvvm.data.api.TheMovieDBInterface
+import com.example.themoviemvvm.data.repository.MovieDetailsNetworkDataSource
+import com.example.themoviemvvm.data.repository.NetworkState
+import com.example.themoviemvvm.data.value_object.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository(private val apiService : TheMovieDBInterface) {
+
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails (compositeDisposable: CompositeDisposable, movieId: Int) : LiveData<MovieDetails> {
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService, compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieResponse
+    }
+
+    fun getMovieDetailsNetworkState(): LiveData<NetworkState> {
+        return movieDetailsNetworkDataSource.networkState
+    }
+}
